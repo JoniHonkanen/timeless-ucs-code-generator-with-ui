3409672eeca58128a9beaefc974cbc45781d9ef3
Joni Joonas Honkanen <joni.j.honkanen@tuni.fi>
2025-02-18 14:05:35 +0200

    remove generated files/folders if exists

--- agents/code_generator_agent.py
+++ agents/code_generator_agent.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 from .common import llm_code
 from schemas import GraphState, Codes
 from prompts.prompts import CODE_GENERATOR_AGENT_PROMPT
@@ -8,6 +10,17 @@ from langchain_core.messages import AIMessage
 # -> Generate code based on user inputs
 async def code_generator_agent(state: GraphState) -> GraphState:
     print("\n**CODE GENERATOR AGENT**")
+    # This deletes all files and subdirectories except the "UI" folder.
+    src_folder = os.path.join("generated", "src")
+    if os.path.exists(src_folder):
+        for item in os.listdir(src_folder):
+            item_path = os.path.join(src_folder, item)
+            if item != "ui":  # Preserve the UI folder
+                if os.path.isdir(item_path):
+                    shutil.rmtree(item_path)
+                else:
+                    os.remove(item_path)
+
     requirement = state["messages"][0].content
     prompt = CODE_GENERATOR_AGENT_PROMPT.format(requirement=requirement)
     structured_llm = llm_code.with_structured_output(Codes)
